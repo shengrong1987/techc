f4fee9919f1732196d2b19df4937db9608e539fe
ShengRong <aimbebe.r@gmail.com>
2015-04-19 17:41:58 -0700

    init framework

---
+++ Techc/Techc/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Techc
+//
+//  Created by sheng rong on 15/4/19.
+//  Copyright (c) 2015年 Techc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
